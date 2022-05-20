439a1d606065df39d4cc497d4873321f73e31567
Phong Tran <phongtran@kidkare.com>
2022-05-20 11:19:55 +0700

    Initial Commit

---
+++ Infrastruture/Data/VehicleDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+
+namespace Infrastruture.Data
+{
+    public class VehicleDBContext : DbContext
+    {
+        public VehicleDBContext(DbContextOptions<VehicleDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ VehicleSupport/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Core.Entities;
+using Infrastruture.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace VehicleSupport.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly VehicleDBContext _context;
+
+        public ProductController(VehicleDBContext context)
+        {
+            _context=context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
